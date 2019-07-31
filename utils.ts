593218d82c98318e53baba2a3308716ebfee06e8
Dmitry Bufistov <dmitry@midokura.com>
2019-07-31 17:53:40 +0100

    Use correct WordArray type.
    WordArray is a strange beast, that I hope is never used in
    calculations. Since CryptoJS is pure java script library we are not
    getting any compiler errors.
    
    This patch make all functions use the type the cryptoJS hash
    functions actually return.
    
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- utils.ts
+++ utils.ts
@@ -6,10 +6,12 @@ import { SRPParameters } from "./parameters";
 
 const identity = <T>(a: T) => a;
 
-export const bigIntegerToWordArray = (n: BigInteger): CryptoJS.WordArray =>
+export type HashWordArray = CryptoJS.LibWordArray;
+
+export const bigIntegerToWordArray = (n: BigInteger): HashWordArray =>
   CryptoJS.enc.Hex.parse(evenLengthHex(n.toString(16)));
 
-export const wordArrayToBigInteger = (words: CryptoJS.WordArray): BigInteger =>
+export const wordArrayToBigInteger = (words: HashWordArray): BigInteger =>
   new BigInteger(CryptoJS.enc.Hex.stringify(words), 16);
 
 export function evenLengthHex(hex: string): string {
@@ -21,10 +23,10 @@ export function evenLengthHex(hex: string): string {
 }
 
 /**
- * Convert some string into CryptoJS.WordArray, suitable for hashing.
+ * Convert some string into LibWordArray, suitable for hashing.
  * @param str Any string, like a username, email, or password
  */
-export function stringToWordArray(str: string): CryptoJS.WordArray {
+export function stringToWordArray(str: string): HashWordArray {
   const bytes = new Uint8Array(str.length);
   for (let i = 0; i < str.length; ++i) {
     bytes[i] = str.charCodeAt(i);
@@ -40,8 +42,8 @@ export function stringToWordArray(str: string): CryptoJS.WordArray {
 }
 
 const padWordArray = (targetLength: number) => (
-  words: CryptoJS.WordArray,
-): CryptoJS.WordArray => {
+  words: HashWordArray,
+): HashWordArray => {
   const hexString = CryptoJS.enc.Hex.stringify(words);
   const currentByteLength = hexString.length / 2;
   const byteLengthDiff = targetLength - currentByteLength;
@@ -55,26 +57,26 @@ const padWordArray = (targetLength: number) => (
 
 export function hash(
   parameters: SRPParameters,
-  ...as: CryptoJS.WordArray[]
-): CryptoJS.WordArray {
+  ...as: HashWordArray[]
+): HashWordArray {
   return hashPadded(parameters, null, ...as);
 }
 
 export function hashPadded(
   parameters: SRPParameters,
   targetLen: number | null,
-  ...as: CryptoJS.WordArray[]
-): CryptoJS.WordArray {
+  ...as: HashWordArray[]
+): HashWordArray {
   parameters.H.reset();
 
   as.map(targetLen !== null ? padWordArray(targetLen) : identity).forEach(
-    (wa: CryptoJS.WordArray) => parameters.H.update(wa),
+    (wa: HashWordArray) => parameters.H.update(wa),
   );
 
   return parameters.H.finalize();
 }
 
-const generateRandom = (numBytes: number = 16): CryptoJS.WordArray => {
+const generateRandom = (numBytes: number = 16): HashWordArray => {
   return CryptoJS.lib.WordArray.random(numBytes) as any;
 };
 
