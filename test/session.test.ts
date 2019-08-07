345469f87b8dce8664b32b1c160518f6decf0c3a
Dmitry Bufistov <dmitry@midokura.com>
2019-08-07 18:11:20 +0100

    More java nimbusds compatibility tests.
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- test/session.test.ts
+++ test/session.test.ts
@@ -28,40 +28,6 @@ class TestSRPSession extends SRPSession {
   }
 }
 
-test('#SRPSession canary for password that is "uneven" as hex string', (t) => {
-  t.plan(2);
-  const testUsername = "peppapig";
-  const testPassword = "edge00044bc49a26"; // problematic as reported by https://midobugs.atlassian.net/browse/ISS-325
-
-  // salt is generated during signup, and sent to client.step2
-  const salt = new BigInteger("99830900279124036031422484022515311814");
-
-  // verifier is generated during signup, and read from storage to server.step1
-  const verifier = createVerifier(TestConfig, testUsername, salt, testPassword);
-
-  const verifierExpected = new BigInteger(
-    "178562055003946915616288416183950560880175291647374906172196005828739793423130057138448557472796429057819212014" +
-      "4114935584475633589713081017567690708669637848379136792476825170841578549100791737447582125876753901554573933" +
-      "8667640603805500045440488046479293037121037880926546495644470644370531564026094560765922188514892916676817675" +
-      "1700937138435898497110083289317944474559572995934674830428142920649062311891412787734495424754159472009938334" +
-      "3222059032274510862728323448600730355383444838443223455907492540308650768601338619602737301031438936911752125" +
-      "3184029489262122079238259800200292396832028759867302637151706175160538",
-  );
-  t.true(verifier.equals(verifierExpected));
-
-  const serverSession = new SRPServerSession(TestConfig);
-  // server gets identifier from client, salt+verifier from db (from signup)
-  const B = serverSession.step1(testUsername, salt, verifier);
-
-  const clientSession = new SRPClientSession(TestConfig);
-  clientSession.step1(testUsername, testPassword);
-  const { A, M1 } = clientSession.step2(salt, B);
-
-  const M2 = serverSession.step2(A, M1);
-  clientSession.step3(M2);
-  t.pass("Canary test passes");
-});
-
 /**
  * Preconditions:
  * * Server has 'v' and 's' in storage associated with 'I'
